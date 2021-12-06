09a4f43e13f35f47a8850098bc90f01e7bead27b
vikzelikov <vikzelikov@yandex.ru>
2021-12-07 00:29:44 +0300

    initial

---
+++ AR Culture/InitialView.swift
@@ -0,0 +1,20 @@
+//
+//  InitialView.swift
+//  AR Culture
+//
+//  Created by Yegor on 06.12.2021.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitialView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialView()
+    }
+}
